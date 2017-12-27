0671db6c566d4818c3f45e7d8ed84b2d1c51293e
Christopher Dorst <chris@dorst.email>
2017-12-27 11:21:06 -0600

    Adding StringJoiner

---
+++ Common.Functions.JoinStringsWithSpace/StringsJoiner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Functions.JoinStringsWithSpace
+{
+    public static class StringsJoiner
+    {
+        private const char Space = ' ';
+
+        public static string JoinWithSpace(IEnumerable<string> strings)
+            => string.Join(Space, strings);
+    }
+}
